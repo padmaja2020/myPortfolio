66a480fb8eceec1bf9ae14f77e8af2381b975bae
padmaja2020 <pvemparala@hotymail.com>
2021-03-17 09:20:25 -0700

    Added Case Study and Updated Resume

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,24 +1,3 @@
-/**	
-	* Template Name: Kevin
-	* Version: 1.0	
-	* Template Scripts
-	* Author: MarkUps
-	* Author URI: http://www.markups.io/
-
-	Custom JS
-	
-	1. FIXED MENU
-	2. FEATURED SLIDE ( TYPED SLIDER )
-	3. SKILL PROGRESS BAR
-	4. MENU SMOOTH SCROLLING
-	5. MOBILE MENU CLOSE  
-	6. PORTFOLIO GALLERY
-	7. PORTFOLIO POPUP VIEW ( IMAGE LIGHTBOX )
-	8. CLIENT TESTIMONIALS ( SLICK SLIDER )
-	9. BUTTON SMOOTH SCROLL ( VIEW MY WORK )
-	
-**/
-
 
 
 (function( $ ){
@@ -33,62 +12,14 @@
 
     if ($(window).scrollTop() > 100) {
 
-        $('#mu-header').addClass('mu-fixed-nav');
+        $('#header').addClass('fixed-nav');
         
 	    } else {
-	        $('#mu-header').removeClass('mu-fixed-nav');
+	        $('#header').removeClass('fixed-nav');
 	    }
 	});
 
-		
-	/* ----------------------------------------------------------- */
-	/*  2. FEATURED SLIDE (TYPED SLIDER)
-	/* ----------------------------------------------------------- */
-
-		var typed = new Typed('#typed', {
-		    stringsElement: '#typed-strings',
-		    typeSpeed: 20,
-		    backSpeed: 20,
-		    startDelay: 1000,
-		    loop: true,
-		    loopCount: Infinity
-		});
-
-		
-	/* ----------------------------------------------------------- */
-	/*  3. SKILL PROGRESS BAR
-	/* ----------------------------------------------------------- */
-
-		$('.mu-skill-progress-bar').appear(function() {
-
-		 	$('.mu-html5-bar').LineProgressbar({
-				percentage: 95,
-				triggerOnce: true
-			});
-
-			$('.mu-css-bar').LineProgressbar({
-				percentage: 90,
-				triggerOnce: true
-			});
-
-			$('.mu-photoshop-bar').LineProgressbar({
-				percentage: 85,
-				triggerOnce: true
-			});
-
-			$('.mu-wordpress-bar').LineProgressbar({
-				percentage: 80,
-				triggerOnce: true
-			});
-
-			$('.mu-jquery-bar').LineProgressbar({
-				percentage: 55,
-				triggerOnce: true
-			});
-
-		});
-
-
+	
 
 	/* ----------------------------------------------------------- */
 	/*  4. MENU SMOOTH SCROLLING
@@ -98,7 +29,7 @@
 
 		// Cache selectors
 		var lastId,
-		topMenu = $(".mu-menu"),
+		topMenu = $(".menu"),
 		topMenuHeight = topMenu.outerHeight()+13,
 		// All list items
 		menuItems = topMenu.find('a[href^=\\#]'),
@@ -147,47 +78,15 @@
 	/*  5. MOBILE MENU CLOSE 
 	/* ----------------------------------------------------------- */ 
 
-		jQuery('.mu-menu').on('click', 'li a', function() {
+		jQuery('.menu').on('click', 'li a', function() {
 		  $('.in').collapse('hide');
 		});
 
 
-	/* ----------------------------------------------------------- */
-	/*  6. PORTFOLIO GALLERY
-	/* ----------------------------------------------------------- */ 
-		$('.filtr-container').filterizr();
+	
 
-		//Simple filter controls
 
-	    $('.mu-simplefilter li').click(function() {
-	        $('.mu-simplefilter li').removeClass('active');
-	        $(this).addClass('active');
-	    });
-
-	/* ----------------------------------------------------------- */
-	/*  7. PORTFOLIO POPUP VIEW ( IMAGE LIGHTBOX )
-	/* ----------------------------------------------------------- */ 
-
-	$('.mu-filter-imglink').magnificPopup({
-	  type: 'image',
-	  mainClass: 'mfp-fade',
-	  gallery:{
-	    enabled:true
-	  }
-	});
-
-	/* ----------------------------------------------------------- */
-	/*  8. CLIENT TESTIMONIALS (SLICK SLIDER)
-	/* ----------------------------------------------------------- */
-
-		$('.mu-testimonial-slide').slick({
-		  arrows: false,
-		  dots: true,
-		  infinite: true,
-		  speed: 500,
-		  autoplay: true,
-		  cssEase: 'linear'
-		});
+	
 
 
 	/* ----------------------------------------------------------- */
